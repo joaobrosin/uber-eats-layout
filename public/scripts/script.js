acdad3f4ad1f4f70aaca13889a68c7d907bcdc03
João Brosin <joao.brosin@gmail.com>
2020-08-28 15:35:50 -0300

    first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,50 @@
+// modal menu 
+
+const openMenuBtn = document.querySelector('.menu-open-btn')
+const closeMenuBtn = document.querySelector('.menu-close-btn')
+const modalMenu = document.querySelector('.modal-menu')
+
+openMenuBtn.addEventListener('click', () => {
+    modalMenu.classList.add('active')
+})
+
+closeMenuBtn.addEventListener('click', () => {
+    modalMenu.classList.remove('active')
+})
+
+// modal video
+
+const openVideoBtn = document.querySelector('.play-btn')
+const videoBackground = document.querySelector('.video-background')
+const videoUrl = document.querySelector('iframe')
+
+openVideoBtn.addEventListener('click', () => {
+    videoBackground.classList.add('active')
+    videoUrl.setAttribute('src', 'https://www.youtube-nocookie.com/embed/7gquYRxLMFI')
+
+})
+
+videoBackground.addEventListener('click', () => {
+    videoBackground.classList.remove('active')
+    videoUrl.setAttribute('src', '')
+})
+
+// scroll transition 
+
+const sections = document.querySelectorAll('.js-scroll')
+const activeScreenHeight = window.innerHeight * 0.7
+
+sections[0].classList.add('active')
+
+function animeScroll () {
+    sections.forEach((section) => {
+        if (section.getBoundingClientRect().top < activeScreenHeight) {
+            section.classList.add('active')
+        } else {
+            section.classList.remove('active')
+        }
+    })
+}
+
+window.addEventListener('scroll', animeScroll)
+
